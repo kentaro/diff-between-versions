8ebb9024de618313f6c705f1b4a33561b8079ebf
Kentaro Kuribayashi <kentarok@gmail.com>
2020-10-16 22:25:39 +0900

    swiped existing artifacts to initialize this repo

--- lib/diff/between/versions/version.rb
+++
@@ -1,7 +0,0 @@
-module Diff
-  module Between
-    module Versions
-      VERSION = "0.1.0"
-    end
-  end
-end
--- test/test_helper.rb
+++
@@ -1,4 +0,0 @@
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "diff/between/versions"
-
-require "minitest/autorun"
